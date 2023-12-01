d8a814570a755596b6ca374211a771756c244b79
PeterDaGrape <petervine1@icloud.com>
2023-12-01 14:12:50 +0000

    upload

---
+++ shaders/Test2/Test2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Test2
+//
+//  Created by Peter Vine on 29/11/2023.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ shaders/test1/test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Peter Vine on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
